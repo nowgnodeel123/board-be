45181dedcacf60268a547a7f548e2eb4db496c85
nowgnodeel <168517856+nowgnodeel123@users.noreply.github.com>
2025-09-11 00:44:49 +0900

    feat: Timestamped 추가

--- src/main/java/com/nowgnodeel/boardbe/BoardBeApplication.java
+++ src/main/java/com/nowgnodeel/boardbe/BoardBeApplication.java
@@ -2,7 +2,9 @@ package com.nowgnodeel.boardbe;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
+@EnableJpaAuditing
 @SpringBootApplication
 public class BoardBeApplication {
 
