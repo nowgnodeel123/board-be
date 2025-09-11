20daca5bbb197e4ff01681abbb22c6f261bcb846
nowgnodeel <168517856+nowgnodeel123@users.noreply.github.com>
2025-09-11 22:46:04 +0900

    feat: Board Service 생성

---
+++ src/main/java/com/nowgnodeel/boardbe/board/service/BoardService.java
@@ -0,0 +1,9 @@
+package com.nowgnodeel.boardbe.board.service;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
+public class BoardService {
+}
