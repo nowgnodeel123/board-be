650ac2633ea378ab2b301dd4f3352509613d497e
nowgnodeel <168517856+nowgnodeel123@users.noreply.github.com>
2025-09-11 22:45:49 +0900

    feat: Board 테이블 생성

---
+++ src/main/java/com/nowgnodeel/boardbe/board/entity/Board.java
@@ -0,0 +1,24 @@
+package com.nowgnodeel.boardbe.board.entity;
+
+import com.nowgnodeel.boardbe.global.entity.Timestamped;
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Getter
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+public class Board extends Timestamped {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    @Column(nullable = false)
+    private String title;
+    @Column(nullable = false)
+    private String description;
+}
