e1095925257edbd55b6f4de7a7b29b4c900620a1
nowgnodeel <168517856+nowgnodeel123@users.noreply.github.com>
2025-09-11 22:50:09 +0900

    modify: Board RestController로 변경

--- src/main/java/com/nowgnodeel/boardbe/board/controller/BoardController.java
+++ src/main/java/com/nowgnodeel/boardbe/board/controller/BoardController.java
@@ -1,10 +1,10 @@
 package com.nowgnodeel.boardbe.board.controller;
 
 import lombok.RequiredArgsConstructor;
-import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
-@Controller
+@RestController
 @RequiredArgsConstructor
 @RequestMapping("/boards")
 public class BoardController {
