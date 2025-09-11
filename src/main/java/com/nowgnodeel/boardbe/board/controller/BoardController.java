39103b3959c9677104084e0a53b8ae63d5b7b570
nowgnodeel <168517856+nowgnodeel123@users.noreply.github.com>
2025-09-11 22:47:44 +0900

    add: Board Controller 생성

---
+++ src/main/java/com/nowgnodeel/boardbe/board/controller/BoardController.java
@@ -0,0 +1,11 @@
+package com.nowgnodeel.boardbe.board.controller;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequiredArgsConstructor
+@RequestMapping("/boards")
+public class BoardController {
+}
