9b48c92803b1370a368e43753eb45c08871e1cd1
nowgnodeel <168517856+nowgnodeel123@users.noreply.github.com>
2025-09-15 20:47:42 +0900

    modify: comment mapping

--- src/main/java/com/nowgnodeel/boardbe/board/entity/Board.java
+++ src/main/java/com/nowgnodeel/boardbe/board/entity/Board.java
@@ -1,11 +1,12 @@
 package com.nowgnodeel.boardbe.board.entity;
 
+import com.nowgnodeel.boardbe.comment.entity.Comment;
 import com.nowgnodeel.boardbe.global.entity.Timestamped;
 import jakarta.persistence.*;
-import lombok.AllArgsConstructor;
-import lombok.Builder;
-import lombok.Getter;
-import lombok.NoArgsConstructor;
+import lombok.*;
+
+import java.util.ArrayList;
+import java.util.List;
 
 @Entity
 @Getter
@@ -17,8 +18,15 @@ public class Board extends Timestamped {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
+
     @Column(nullable = false)
     private String title;
+
     @Column(nullable = false)
-    private String description;
+    private String content;
+
+    @OneToMany(fetch = FetchType.LAZY, cascade = CascadeType.ALL)
+    @JoinColumn(name = "board_id")
+    @ToString.Exclude
+    private List<Comment> comments = new ArrayList<>();
 }
