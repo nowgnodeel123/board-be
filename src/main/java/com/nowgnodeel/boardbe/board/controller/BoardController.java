400a726f86486cc879659cbf7f8a25f521886510
nowgnodeel <168517856+nowgnodeel123@users.noreply.github.com>
2025-09-16 20:13:45 +0900

    feat: on create function

--- src/main/java/com/nowgnodeel/boardbe/board/controller/BoardController.java
+++ src/main/java/com/nowgnodeel/boardbe/board/controller/BoardController.java
@@ -1,6 +1,11 @@
 package com.nowgnodeel.boardbe.board.controller;
 
+import com.nowgnodeel.boardbe.board.dto.CreateBoardRequestDto;
+import com.nowgnodeel.boardbe.board.service.BoardService;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -8,4 +13,11 @@ import org.springframework.web.bind.annotation.RestController;
 @RequiredArgsConstructor
 @RequestMapping("/boards")
 public class BoardController {
+
+    private final BoardService boardService;
+
+    @PostMapping
+    public ResponseEntity<String> createBoard(@RequestBody CreateBoardRequestDto requestDto) {
+        return boardService.createBoard(requestDto);
+    }
 }
--- src/main/java/com/nowgnodeel/boardbe/board/service/BoardService.java
+++ src/main/java/com/nowgnodeel/boardbe/board/service/BoardService.java
@@ -1,9 +1,28 @@
 package com.nowgnodeel.boardbe.board.service;
 
+import com.nowgnodeel.boardbe.board.dto.CreateBoardRequestDto;
+import com.nowgnodeel.boardbe.board.entity.Board;
+import com.nowgnodeel.boardbe.board.repository.BoardRepository;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 @Service
 @RequiredArgsConstructor
 public class BoardService {
+
+    private final BoardRepository boardRepository;
+
+    @Transactional
+    public ResponseEntity<String> createBoard(CreateBoardRequestDto requestDto) {
+        Board board = Board.builder()
+                .title(requestDto.title())
+                .content(requestDto.content())
+                .build();
+
+        boardRepository.save(board);
+
+        return ResponseEntity.ok().body("Board created successfully");
+    }
 }
