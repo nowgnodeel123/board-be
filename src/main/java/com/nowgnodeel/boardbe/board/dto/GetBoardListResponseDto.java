5871c2bc025f6fc5de40b8d0e6e2f233cb566844
nowgnodeel <168517856+nowgnodeel123@users.noreply.github.com>
2025-09-18 21:31:08 +0900

    add: add entity to dto method

--- src/main/java/com/nowgnodeel/boardbe/board/dto/GetBoardListResponseDto.java
+++ src/main/java/com/nowgnodeel/boardbe/board/dto/GetBoardListResponseDto.java
@@ -1,7 +1,18 @@
 package com.nowgnodeel.boardbe.board.dto;
 
+import com.nowgnodeel.boardbe.board.common.Category;
+import com.nowgnodeel.boardbe.board.entity.Board;
+
 public record GetBoardListResponseDto(
         String title,
-        String content
+        String content,
+        Category category
 ) {
+    public static GetBoardListResponseDto from(Board board) {
+        return new GetBoardListResponseDto(
+                board.getTitle(),
+                board.getContent(),
+                board.getCategory()
+        );
+    }
 }
