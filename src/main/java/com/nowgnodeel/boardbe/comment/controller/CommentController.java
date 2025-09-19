b10fa0a7bf1a9f4cf0d2b23ce5e3ea7362d2f88d
nowgnodeel <168517856+nowgnodeel123@users.noreply.github.com>
2025-09-19 23:03:41 +0900

    feat: add function createComment

---
+++ src/main/java/com/nowgnodeel/boardbe/comment/controller/CommentController.java
@@ -0,0 +1,29 @@
+package com.nowgnodeel.boardbe.comment.controller;
+
+import com.nowgnodeel.boardbe.comment.dto.CreateCommentRequestDto;
+import com.nowgnodeel.boardbe.comment.dto.CreateCommentResponseDto;
+import com.nowgnodeel.boardbe.comment.entity.Comment;
+import com.nowgnodeel.boardbe.comment.service.CommentService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.net.URI;
+
+@RestController
+@RequiredArgsConstructor
+@RequestMapping("/boards/{boardId}/comments")
+public class CommentController {
+
+    private final CommentService commentService;
+
+    @PostMapping
+    public ResponseEntity<CreateCommentResponseDto> createComment(
+            @PathVariable("boardId") Long boardId,
+            @RequestBody CreateCommentRequestDto requestDto) {
+        Comment createComment = commentService.createComment(boardId, requestDto);
+        CreateCommentResponseDto responseDto = CreateCommentResponseDto.from(createComment);
+        URI location = URI.create("/comments/" + createComment.getId());
+        return ResponseEntity.created(location).body(responseDto);
+    }
+}
---
+++ src/main/java/com/nowgnodeel/boardbe/comment/service/CommentService.java
@@ -0,0 +1,29 @@
+package com.nowgnodeel.boardbe.comment.service;
+
+import com.nowgnodeel.boardbe.board.entity.Board;
+import com.nowgnodeel.boardbe.board.repository.BoardRepository;
+import com.nowgnodeel.boardbe.comment.dto.CreateCommentRequestDto;
+import com.nowgnodeel.boardbe.comment.entity.Comment;
+import com.nowgnodeel.boardbe.comment.repository.CommentRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+@Service
+@RequiredArgsConstructor
+public class CommentService {
+
+    private final CommentRepository commentRepository;
+    private final BoardRepository boardRepository;
+
+    @Transactional
+    public Comment createComment(Long boardId, CreateCommentRequestDto requestDto) {
+        Board board = boardRepository.findById(boardId)
+                .orElseThrow(() -> new IllegalArgumentException("존재하지 않는 게시글입니다."));
+        Comment comment = Comment.builder()
+                .comment(requestDto.comment())
+                .build();
+        board.addComment(comment);
+        return comment;
+    }
+}
