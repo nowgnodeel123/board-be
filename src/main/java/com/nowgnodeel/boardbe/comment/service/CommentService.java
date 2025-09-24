083244f5c550a25c4df4f72d0abcad4c4836421c
nowgnodeel <168517856+nowgnodeel123@users.noreply.github.com>
2025-09-24 22:07:40 +0900

    feat: add function on get comment list

--- src/main/java/com/nowgnodeel/boardbe/comment/service/CommentService.java
+++ src/main/java/com/nowgnodeel/boardbe/comment/service/CommentService.java
@@ -3,10 +3,13 @@ package com.nowgnodeel.boardbe.comment.service;
 import com.nowgnodeel.boardbe.board.entity.Board;
 import com.nowgnodeel.boardbe.board.repository.BoardRepository;
 import com.nowgnodeel.boardbe.comment.dto.CreateCommentRequestDto;
+import com.nowgnodeel.boardbe.comment.dto.GetCommentListResponseDto;
 import com.nowgnodeel.boardbe.comment.dto.UpdateCommentRequestDto;
 import com.nowgnodeel.boardbe.comment.entity.Comment;
 import com.nowgnodeel.boardbe.comment.repository.CommentRepository;
 import lombok.RequiredArgsConstructor;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
@@ -19,8 +22,7 @@ public class CommentService {
 
     @Transactional
     public Comment createComment(Long boardId, CreateCommentRequestDto requestDto) {
-        Board board = boardRepository.findById(boardId)
-                .orElseThrow(() -> new IllegalArgumentException("존재하지 않는 게시글입니다."));
+        Board board = boardRepository.findById(boardId).orElseThrow(() -> new IllegalArgumentException("존재하지 않는 게시글입니다."));
         Comment comment = Comment.builder()
                 .comment(requestDto.comment())
                 .build();
@@ -30,8 +32,7 @@ public class CommentService {
 
     @Transactional
     public void deleteComment(Long boardId, Long commentId) {
-        boardRepository.findById(boardId)
-                .orElseThrow(() -> new RuntimeException("Board not found"));
+        boardRepository.findById(boardId).orElseThrow(() -> new RuntimeException("Board not found"));
         Comment comment = commentRepository.findById(commentId)
                 .orElseThrow(() -> new IllegalArgumentException("Comment not found"));
         commentRepository.deleteById(comment.getId());
@@ -39,10 +40,16 @@ public class CommentService {
 
     @Transactional
     public Comment updateComment(Long boardId, Long commentId, UpdateCommentRequestDto requestDto) {
-        boardRepository.findById(boardId)
-                .orElseThrow(() -> new RuntimeException("Board not found"));
+        boardRepository.findById(boardId).orElseThrow(() -> new RuntimeException("Board not found"));
         Comment comment = commentRepository.findById(commentId).orElseThrow(() -> new RuntimeException("Comment not found"));
         comment.patch(requestDto);
         return comment;
+    }
+
+    @Transactional(readOnly = true)
+    public Page<GetCommentListResponseDto> getAllComments(Long boardId, Pageable pageable) {
+        boardRepository.findById(boardId).orElseThrow(() -> new RuntimeException("Board not found"));
+        Page<Comment> commentPage = commentRepository.findByBoardId(boardId, pageable);
+        return commentPage.map(GetCommentListResponseDto::from);
     }
 }
