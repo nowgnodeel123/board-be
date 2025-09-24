9d9992633165f38210996889fca45d22c21fb406
nowgnodeel <168517856+nowgnodeel123@users.noreply.github.com>
2025-09-24 22:07:58 +0900

    add : find by board id

--- src/main/java/com/nowgnodeel/boardbe/comment/repository/CommentRepository.java
+++ src/main/java/com/nowgnodeel/boardbe/comment/repository/CommentRepository.java
@@ -1,9 +1,13 @@
 package com.nowgnodeel.boardbe.comment.repository;
 
 import com.nowgnodeel.boardbe.comment.entity.Comment;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
 public interface CommentRepository extends JpaRepository<Comment, Long> {
+
+    Page<Comment> findByBoardId(Long boardId, Pageable pageable);
 }
