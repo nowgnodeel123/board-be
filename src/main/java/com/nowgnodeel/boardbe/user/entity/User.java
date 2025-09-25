13a1d800f6e73f3ef515fbc65aeb9b07cc020ed6
nowgnodeel <168517856+nowgnodeel123@users.noreply.github.com>
2025-09-26 00:42:27 +0900

    add : add User table

---
+++ src/main/java/com/nowgnodeel/boardbe/user/entity/User.java
@@ -0,0 +1,26 @@
+package com.nowgnodeel.boardbe.user.entity;
+
+import com.nowgnodeel.boardbe.global.entity.Timestamped;
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Getter
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+public class User extends Timestamped {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false, unique = true)
+    private String username;
+
+    @Column(nullable = false)
+    private String password;
+}
